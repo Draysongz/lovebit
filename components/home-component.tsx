929a27b6e2764a83f07df6b5dec9a93e8667187c
oyenolaphilipinc <oyenolaphilip89@gmail.com>
2024-12-20 10:09:48 -0800

    done

--- components/home-component.tsx
+++ components/home-component.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
+
 const TapComponent = () => {
     return(
-        <div className="flex gap-2 mt-4">
+        <div className="flex gap-2 pt-6">
             <div className="group relative aspect-square overflow-hidden ml-2">
                 <Image src={"/hand.svg"} alt="Saved" width={100} height={100} className="w-full h-full object-cover"/>
                 <div className="absolute border bg-button-angular bottom-0 left-0 right-0 p-4">
