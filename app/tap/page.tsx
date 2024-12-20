86984053982ea9a0f7433cfc05c28d26ff007f65
oyenolaphilipinc <oyenolaphilip89@gmail.com>
2024-12-20 10:17:13 -0800

    fixed

--- app/tap/page.tsx
+++ app/tap/page.tsx
@@ -3,7 +3,6 @@ import { useState, useCallback, useRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { BottomNav } from "@/components/button-nav"
 import Image from "next/image"
-import TapComponent from '@/components/home-component'
 
 export default function OpenScreen() {
   const [count, setCount] = useState(134690)
@@ -31,7 +30,26 @@ export default function OpenScreen() {
 
   return (
     <div className="min-h-screen bg-[url('/darkbg.svg')] bg-cover bg-center  flex flex-col items-center">
-      <TapComponent />
+      <div className="flex gap-2 pt-6">
+                  <div className="group relative aspect-square overflow-hidden ml-2">
+                      <Image src={"/hand.svg"} alt="Saved" width={100} height={100} className="w-full h-full object-cover"/>
+                      <div className="absolute border bg-button-angular bottom-0 left-0 right-0 p-4">
+                      <h3 className="text-xs text-center font-semibold text-white">POLAR BEAR</h3>
+                      </div>
+                  </div>
+                  <div className="group relative aspect-square overflow-hidden">
+                      <Image src={"/donation.svg"} alt="Saved" width={100} height={100} className="w-full h-full object-cover"/>
+                      <div className="absolute border bg-button-angular bottom-0 left-0 right-0 p-4">
+                      <h3 className="text-xs text-center font-semibold text-white">DONATION</h3>
+                      </div>
+                  </div>
+                  <div className="group relative aspect-square overflow-hidden mr-2">
+                      <Image src={"/bear.svg"} alt="Saved" width={100} height={100} className="w-full h-full object-cover"/>
+                      <div className="absolute border bg-button-angular bottom-0 left-0 right-0 p-4">
+                      <h3 className="text-xs text-center font-semibold text-white">LOVE BIT</h3>
+                      </div>
+                  </div>
+              </div>
       <div className="flex gap-4 mt-6">
         <Image src="/btclogo.svg" width={60} height={60} alt="btclogo" />
         <p className="text-5xl text-white font-bold mt-2">{count.toLocaleString()}</p>
