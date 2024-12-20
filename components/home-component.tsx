3f8a1aafa5969433e1366c4a84de3b66a64d083f
oyenolaphilipinc <oyenolaphilip89@gmail.com>
2024-12-20 03:47:04 -0800

    fixed

--- components/home-component.tsx
+++ components/home-component.tsx
@@ -5,19 +5,19 @@ const TapComponent = () => {
             <div className="group relative aspect-square overflow-hidden ml-2">
                 <Image src={"/hand.svg"} alt="Saved" width={100} height={100} className="w-full h-full object-cover"/>
                 <div className="absolute border bg-button-angular bottom-0 left-0 right-0 p-4">
-                <h3 className="text-xs text-center font-semibold text-black">POLAR BEAR</h3>
+                <h3 className="text-xs text-center font-semibold text-white">POLAR BEAR</h3>
                 </div>
             </div>
             <div className="group relative aspect-square overflow-hidden">
                 <Image src={"/donation.svg"} alt="Saved" width={100} height={100} className="w-full h-full object-cover"/>
                 <div className="absolute border bg-button-angular bottom-0 left-0 right-0 p-4">
-                <h3 className="text-xs text-center font-semibold text-black">DONATION</h3>
+                <h3 className="text-xs text-center font-semibold text-white">DONATION</h3>
                 </div>
             </div>
             <div className="group relative aspect-square overflow-hidden mr-2">
                 <Image src={"/bear.svg"} alt="Saved" width={100} height={100} className="w-full h-full object-cover"/>
                 <div className="absolute border bg-button-angular bottom-0 left-0 right-0 p-4">
-                <h3 className="text-xs text-center font-semibold text-black">LOVE BIT</h3>
+                <h3 className="text-xs text-center font-semibold text-white">LOVE BIT</h3>
                 </div>
             </div>
         </div>
